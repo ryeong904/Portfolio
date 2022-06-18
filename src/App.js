cb2ca21b091b68408ab469c07fa48ac629c8b89d
HyeRyeong <fud3725@naver.com>
2022-06-18 13:44:17 +0900

    fullpage 적용

--- src/App.js
+++ src/App.js
@@ -2,16 +2,26 @@ import './App.css';
 import { HeaderTag } from './components/Header';
 import { Home } from './components/Home';
 import { DownButton } from './components/DownButton';
-import ReactFullpage from '@fullpage/react-fullpage';
 import styled from 'styled-components';
-
+import { FullPage, Slide } from 'react-full-page';
 function App() {
   return (
-    <Container>
-      <HeaderTag></HeaderTag>
-      <Home></Home>
-      <DownButton />
-    </Container>
+    <FullPage>
+      <Slide>
+        <Container>
+          <HeaderTag></HeaderTag>
+          <Home></Home>
+          <DownButton />
+        </Container>
+      </Slide>
+      <Slide>
+        <Container>
+          <HeaderTag></HeaderTag>
+          <Home></Home>
+          <DownButton />
+        </Container>
+      </Slide>
+    </FullPage>
   );
 }
 
