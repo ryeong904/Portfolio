bd697cb71e7c5962d9e790e28aa08e12e2819a85
HyeRyeong <fud3725@naver.com>
2022-06-26 16:24:35 +0900

    chart.js 사용한 도넛 그래프

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -9,6 +9,40 @@ const list = (array) => {
   return array.map((data) => <p key={data}>{data}</p>);
 };
 
+function sortFunc(array) {
+  const sortedList = [];
+
+  for (let key in array) {
+    sortedList.push([key, array[key]]);
+  }
+
+  sortedList.sort(function (a, b) {
+    return b[1] - a[1];
+  });
+  return sortedList;
+}
+
+async function getLanguageList(array) {
+  const newList = array.map((data) => data.languages_url);
+  const result = {};
+
+  for (let i = 0; i < newList.length; i++) {
+    const res = await fetch(newList[i]);
+    const temp = await res.json();
+
+    const arr = Object.keys(temp);
+    arr.forEach((data) => {
+      if (result.hasOwnProperty(data)) {
+        const sum = result[data] + temp[data];
+        result[data] = sum;
+      } else {
+        result[data] = temp[data];
+      }
+    });
+  }
+  return sortFunc(result);
+}
+
 export function Skills() {
   const frontArray = ['HTML', 'CSS', 'JavaScript', 'jQuery', 'React'];
   const backArray = ['Node.js', 'Express.js', 'PHP', 'FastAPI'];
@@ -16,66 +50,60 @@ export function Skills() {
   const mobileArray = ['Java', 'Android'];
   const vcsArray = ['GitHub', 'GitLab'];
 
-  const [data, setData] = useState([]);
+  const [chartData, setChartData] = useState({
+    datasets: [
+      {
+        label: 'Gihub Data',
+        data: [],
+        backgroundColor: [
+          'rgb(255, 99, 132, 0.9)',
+          'rgb(255, 205, 86,0.9)',
+          'rgb(255, 159, 64,0.9)',
+          'rgb(75, 192, 192,0.9)',
+          'rgb(48, 161, 237,0.9)',
+        ],
+        hoverOffset: 4,
+      },
+    ],
+  });
+  const [flag, setFlag] = useState(false);
 
   useEffect(() => {
     fetch('https://api.github.com/users/ryeong904/repos')
       .then((res) => res.json())
       .then(async (datas) => {
-        const d = await getLanguageList(datas);
-        setData(d);
-        console.log(data);
+        const result = await getLanguageList(datas);
+        const temp = result.slice(0, 5);
+        const labels = temp.map((e) => e[0]);
+        const statistics = temp.map((e) => {
+          return e[1];
+        });
+
+        setChartData((current) => {
+          const newObj = { ...current };
+          newObj.labels = labels;
+          newObj.datasets[0].data = statistics;
+          setFlag(true);
+          return newObj;
+        });
       })
       .catch((err) => console.log(err));
   }, []);
 
-  async function getLanguageList(array) {
-    const newList = array.map((data) => data.languages_url);
-    const result = {};
-
-    for (let i = 0; i < newList.length; i++) {
-      const res = await fetch(newList[i]);
-      const temp = await res.json();
-
-      const arr = Object.keys(temp);
-      arr.forEach((data) => {
-        if (result.hasOwnProperty(data)) {
-          const sum = result[data] + temp[data];
-          result[data] = sum;
-        } else {
-          result[data] = temp[data];
-        }
-      });
-    }
-    const sortedList = [];
-
-    for (let key in result) {
-      sortedList.push([key, result[key]]);
-    }
-
-    sortedList.sort(function (a, b) {
-      return b[1] - a[1];
-    });
-    return sortedList;
-  }
-
   const options = {
-    reponsive: false,
-  };
-  const a = {
-    labels: ['Red', 'Blue', 'Yellow'],
-    datasets: [
-      {
-        label: 'My First Dataset',
-        data: [300, 50, 100],
-        backgroundColor: [
-          'rgb(255, 99, 132)',
-          'rgb(54, 162, 235)',
-          'rgb(255, 205, 86)',
-        ],
-        hoverOffset: 4,
+    plugins: {
+      legend: {
+        labels: {
+          color: '#262626',
+          font: {
+            size: 12,
+            weight: 600,
+          },
+        },
       },
-    ],
+    },
+    maintainAspectRatio: false,
+    responsive: false,
   };
 
   return (
@@ -104,13 +132,17 @@ export function Skills() {
             <div>{list(vcsArray)}</div>
           </Content>
         </div>
-        <Doughnut
-          data={a}
-          width="300px"
-          height="300px"
-          display="inline"
-          options={{ maintainAspectRatio: false, responsive: false }}
-        />
+        {flag ? (
+          <Doughnut
+            data={chartData}
+            width="300px"
+            height="300px"
+            display="inline"
+            options={options}
+          />
+        ) : (
+          <div>Loading ...</div>
+        )}
       </section>
     </Main>
   );
@@ -126,9 +158,7 @@ const Content = styled.div`
     border-bottom: none;
   }
   div {
-    // width: 75%;
     min-width: 75px;
-    // background-color: white;
     text-align: center;
     display: flex;
     flex-direction: row;
@@ -148,7 +178,6 @@ const Content = styled.div`
     font-weight: 500;
     font-size: 1.4rem;
     margin: 10px 0;
-    // color: #333333;
   }
 `;
 
@@ -160,7 +189,6 @@ const Main = styled.div`
   grid-template-columns: 1fr 1fr;
   height: 100%;
   width: 100%;
-  // background-color: rgb(255, 255, 255);
   margin: 0 auto;
   transition: all 0.3s;
   animation: background 0.5s;
@@ -191,8 +219,7 @@ const Main = styled.div`
   }
 
   section {
-    // width: 490px;
-    width: 50%;
+    width: 800px;
     background-color: rgb(255, 255, 255, 0.2);
     display: flex;
     justify-content: center;
@@ -203,6 +230,6 @@ const Main = styled.div`
   }
 
   section > canvas {
-    // margin-right: 20px;
+    margin-right: 20px;
   }
 `;
