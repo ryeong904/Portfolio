191f1c24cf9402efb72709f405b81dc332033b9d
HyeRyeong <fud3725@naver.com>
2022-10-21 14:48:42 +0900

    [fix] 헤더 작업

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,12 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithubAlt, faBloggerB } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import {
+  faEnvelope,
+  faUser,
+  faFileLines,
+  faKeyboard,
+} from '@fortawesome/free-solid-svg-icons';
 export function Header() {
   return (
     <HeaderWrapper>
@@ -23,15 +28,81 @@ export function Header() {
         </a>
       </IconList>
       <hr style={{ width: 200 }}></hr>
-      <ul style={{ marginTop: 5 }}>
-        <li>About Me</li>
-        <li>Projects</li>
-        <li>Resume</li>
-      </ul>
+      <MenuWrapper>
+        <li>
+          <a href="">
+            <FontAwesomeIcon
+              icon={faUser}
+              size="1x"
+              style={{ marginLeft: 2, marginRight: 15 }}
+            ></FontAwesomeIcon>
+            <span>About Me</span>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <FontAwesomeIcon
+              icon={faKeyboard}
+              size="1x"
+              style={{ marginRight: 14 }}
+            ></FontAwesomeIcon>
+            <span>Projects</span>
+          </a>
+        </li>
+        <li>
+          <a href="pdf/resume.pdf" target="_blank">
+            <FontAwesomeIcon
+              icon={faFileLines}
+              size="1x"
+              style={{ marginLeft: 4, marginRight: 18 }}
+            ></FontAwesomeIcon>
+            <span>Resume</span>
+          </a>
+        </li>
+      </MenuWrapper>
     </HeaderWrapper>
   );
 }
 
+// 카테고리 메뉴 컴포넌트
+const MenuWrapper = styled.ul`
+  margin-top: 5px;
+  list-style: none;
+  padding: 0;
+
+  display: flex;
+  flex-direction: column;
+
+  // 카테고리 메뉴들 가운데로
+  margin: 0 auto;
+
+  // 디테일한 부분 스타일링
+  li {
+    font-size: 18px;
+    font-weight: 800;
+    margin-top: 10px;
+    padding: 5px;
+
+    // 왼쪽 정렬
+    text-align: left;
+
+    // hover시 변화
+    a {
+      svg {
+        margin: 0;
+      }
+      color: white;
+      text-decoration: none;
+    }
+    a:hover {
+      transition: all 0.2s;
+      color: #334735;
+    }
+
+    span {
+    }
+  }
+`;
 // 헤더 테두리. 배경색 설정, 왼쪽에 고정
 const HeaderWrapper = styled.header`
   background-color: #749f82;
